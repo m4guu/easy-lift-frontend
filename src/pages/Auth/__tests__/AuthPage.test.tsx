a4152cb5f9067fc7b260fdfede28324ae5daf063
Krystian Domzalowicz <krystian.domza@gmail.com>
2023-07-05 19:27:32 +0200

    feat: add first test

--- src/pages/Auth/__tests__/AuthPage.test.tsx
+++ src/pages/Auth/__tests__/AuthPage.test.tsx
@@ -1,11 +1,51 @@
-import axios from "axios";
+import { Mock } from "vitest";
+import { fireEvent, render, screen } from "../../../config/tests/custom-render";
 
-// vi.mock("axios");
+import { useUserContext } from "../../../contexts/userContext";
+
+import Auth from "..";
+
+import { Status } from "../../../shared/enums";
+
+vi.mock("../../../../hooks/formHooks/auth/useAuthForm", () => ({
+  useAuthForm: vi.fn().mockReturnValue({
+    methods: {
+      handleSubmit: vi.fn(),
+    },
+    onSubmit: vi.fn(),
+  }),
+}));
+vi.mock("../../../contexts/userContext");
+
+const mockedUseUserContext = useUserContext as Mock;
+// const mockedUseAuthForm = useAuthForm as Mock;
 
 describe("Auth Page", () => {
   describe("registration", () => {
     describe("with valid registration data", () => {
-      it.todo("should redirect to the login board", async () => {});
+      it("should redirect to the login board", () => {
+        mockedUseUserContext.mockReturnValue({
+          isLogging: false,
+          isRegistering: false,
+          registerStatus: Status.IDLE,
+        });
+
+        const { rerender } = render(<Auth />);
+        const signUpTab = screen.getByText("signup");
+        const loginTab = screen.getByText("login");
+
+        // change into registration tab
+        fireEvent.click(signUpTab);
+        // mock
+        mockedUseUserContext.mockReturnValue({
+          isLogging: false,
+          isRegistering: false,
+          registerStatus: Status.SUCCESS,
+        });
+        rerender(<Auth />);
+
+        expect(loginTab).toHaveAttribute("aria-selected", "true");
+      });
       it.todo(
         "should automatically complete the email in the login board",
         async () => {}
@@ -26,6 +66,5 @@ describe("Auth Page", () => {
         it.todo("should redirect to configuration page", async () => {});
       });
     });
-    describe("with invalid login data", () => {});
   });
 });
